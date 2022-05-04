6134c31d85d8506c76f98d98784d06d16cfeae2f
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-05-04 13:07:01 +0100

    add Education section "add/edit/remove" items

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,10 +1,111 @@
 import React, { Component } from 'react';
+import EducationItem from './EducationItem';
+import uniqid from 'uniqid';
 
 class Education extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			editable: true,
+			items: [
+				{
+					id: '1',
+					orgName: 'University of Hull',
+					courseName: 'Maths BSc',
+					startDate: '2000-01-02',
+					endDate: '2005-04-12',
+				},
+			],
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.deleteEntry = this.deleteEntry.bind(this);
+	}
+
+	handleChange = (event) => {
+		const newArr = [...this.state.items];
+		const index = newArr.findIndex(
+			(item) => item.id === event.target.parentNode.dataset.id
+		);
+		const item = newArr[index];
+		item[event.target.name] = event.target.value;
+		newArr.splice(index, 1, item);
+		this.setState({
+			items: newArr,
+		});
+	};
+
+	handleClick(event) {
+		if (event.target.id === 'addNewEducationItem') {
+			const newArr = [...this.state.items];
+			newArr.push({
+				id: uniqid(),
+				orgName: '',
+				courseName: '',
+				startDate: '',
+				endDate: '',
+			});
+			this.setState({
+				items: newArr,
+			});
+		}
+
+		if (event.target.dataset.command === 'delete') {
+			this.deleteEntry(event.target.dataset.id);
+		}
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.setState({
+			editable: !this.state.editable,
+		});
+	};
+
+	deleteEntry = (id) => {
+		const newArr = [...this.state.items];
+		const index = newArr.findIndex((item) => item.id === id);
+		newArr.splice(index, 1);
+		this.setState({
+			items: newArr,
+		});
+	};
+
 	render() {
+		const { editable } = this.state;
+
+		const educationItemsArray = this.state.items.map((item) => {
+			return (
+				<EducationItem
+					key={item.id}
+					data={item}
+					editable={editable}
+					handleClick={this.handleClick}
+					handleChange={this.handleChange}
+				/>
+			);
+		});
+
 		return (
 			<div>
-				<h2>Education</h2>
+				<div className='form-header'>
+					<h2>Education</h2>
+					<button type='button' onClick={this.handleSubmit}>
+						{editable ? 'Save Section' : 'Edit Section'}
+					</button>
+				</div>
+				<div id='educationDiv'> {educationItemsArray} </div>
+				{editable ? (
+					<button
+						type='button'
+						id='addNewEducationItem'
+						onClick={this.handleClick}
+					>
+						Add New Education Item
+					</button>
+				) : null}
 			</div>
 		);
 	}
---
+++ src/components/EducationItem.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+class EducationItem extends Component {
+	render() {
+		const { editable, handleClick, handleChange } = this.props;
+		const { id, orgName, courseName, startDate, endDate } = this.props.data;
+
+		return (
+			<div>
+				<form>
+					<div className='form-input-section' data-id={id}>
+						<label htmlFor='orgName'>
+							Place of Study / Organisation
+						</label>
+						{editable ? (
+							<input
+								id='orgName'
+								type='text'
+								name='orgName'
+								placeholder='Place of Study / Organisation'
+								onChange={handleChange}
+								value={orgName}
+							/>
+						) : orgName ? (
+							orgName
+						) : (
+							'?'
+						)}
+					</div>
+					<div className='form-input-section'>
+						<label htmlFor='courseName'>
+							Course / Qualification
+						</label>
+						{editable ? (
+							<input
+								id='courseName'
+								type='text'
+								name='courseName'
+								placeholder='Course / Qualification'
+								onChange={handleChange}
+								value={courseName}
+							/>
+						) : courseName ? (
+							courseName
+						) : (
+							'?'
+						)}
+					</div>
+					<div className='form-input-section'>
+						<label htmlFor='startDate'>Start Date</label>
+						{editable ? (
+							<input
+								id='startDate'
+								type='date'
+								name='startDate'
+								onChange={handleChange}
+								value={startDate}
+							/>
+						) : startDate ? (
+							startDate
+						) : (
+							'?'
+						)}
+					</div>
+					<div className='form-input-section'>
+						<label htmlFor='endDate'>End Date</label>
+						{editable ? (
+							<input
+								id='endDate'
+								type='date'
+								name='endDate'
+								onChange={handleChange}
+								value={endDate}
+							/>
+						) : endDate ? (
+							endDate
+						) : (
+							'?'
+						)}
+					</div>
+					{editable ? (
+						<button
+							type='button'
+							data-command='delete'
+							data-id={id}
+							onClick={handleClick}
+						>
+							Delete
+						</button>
+					) : null}
+				</form>
+			</div>
+		);
+	}
+}
+
+export default EducationItem;
